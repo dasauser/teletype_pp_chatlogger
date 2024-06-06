49214dc046e532ef14bf0db2cb08e5910946259e
dasauser <dasauser@gmail.com>
2024-06-06 21:48:42 +0300

    Added TeletypeToken class desc

--- src/Component/TeletypeToken.php
+++ src/Component/TeletypeToken.php
@@ -2,6 +2,9 @@
 
 namespace App\Component;
 
+/**
+ * Teletype API access token (string).
+ */
 final class TeletypeToken implements \Stringable
 {
     public function __construct(protected string $token)
